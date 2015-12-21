6a91b176c9bbad8e24adf360e025aaaf2869721e
unknown <asantiago@asantiago-PC.transnetwork.local>
2015-12-21 10:06:20 -0600

    Proyecto v0.1

---
+++ closeRequestPastTime/closeRequestPastTime/App_Code/SqlConexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+//@autor Alfredo Santiago Alvarado
+namespace closeRequestPastTime
+{
+    class SqlConexion
+    {
+         private string connect ;
+        SqlConnection myConnection;
+        SqlCommand cmd;
+        public SqlConexion() //Constructor genera variables para la coneccion 
+        {
+            connect = System.Configuration.ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
+             myConnection = new SqlConnection(connect);
+             cmd = new SqlCommand();
+          
+        }
+
+
+        public List<String> consultaTickets(string query)
+        {
+            List<string> ticketsId = new List<string> () ;
+            SqlDataReader reader ;
+            try
+            {
+               cmd.Connection = myConnection;
+               cmd.CommandText = query;
+               myConnection.Open();
+               reader= cmd.ExecuteReader();
+                while (reader.Read()) {
+                    ticketsId.Add(reader["id"].ToString());
+                }
+            }
+            catch (Exception ex ) {
+                Console.WriteLine(ex);
+            }
+
+            return ticketsId;
+           
+        }
+        
+    }
+}
---
+++ closeRequestPastTime/closeRequestPastTime/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Threading;
+
+/* @Author Alfredo Santiago Alvarado 
+ * 18-12-2015
+ * proceso para cerrar tickets de la aplicacion servicedesk , en cuestion la aplicacion
+ * ejecuta un SP que devuelve los Id de los tickets que se encuentran en status Resolved 
+ * con mas de 5 dias , para posteriormente cerrarlos por medio de el API se Service Desk 
+ * mas info : https://www.manageengine.com/products/service-desk/help/adminguide/api/rest-api.html
+ * 
+ * Actualizaciones  
+ * 
+ * */
+namespace closeRequestPastTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Url, ticket;
+            SqlConexion conn = new SqlConexion();
+            List<string> lista = new List<string>();
+
+            lista = conn.consultaTickets("execute SPticketsPendingClose");
+            HttpUtility enCoding = new HttpUtility();
+
+            foreach (string value in lista)
+            {
+                try
+                {
+                    ticket = value;
+                    Url = "http://helpdesk.transnetwork.com:8383/sdpapi/request/{0}/?TECHNICIAN_KEY=9CA0D982-58CC-456F-9525-CEEF00B5B7E0&OPERATION_NAME=CLOSE_REQUEST";
+                    Url = string.Format(Url, ticket);
+
+                    HttpWebRequest req = WebRequest.Create(Url) as HttpWebRequest;
+                    using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+                    {
+                        StreamReader reader = new StreamReader(resp.GetResponseStream());
+                        string resultado = reader.ReadToEnd();
+                        Console.WriteLine(resultado);
+                    }
+                  
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }                       
+
+
+               
+            } // end foreach 
+        }// end main 
+    }// end class
+}
